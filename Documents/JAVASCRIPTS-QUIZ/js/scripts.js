8ea9c8f37f21c320d9a6d848c7f409175479d633
adho <adhoadhigal>
2019-02-25 12:00:01 +0300

    added remark comments

--- Documents/JAVASCRIPTS-QUIZ/js/scripts.js
+++ Documents/JAVASCRIPTS-QUIZ/js/scripts.js
@@ -12,12 +12,22 @@ $(document).ready(function(){
          var percentage=(total/80)*100;
          $("#percentage").text("Your have "+ percentage + "%");
          $("#results").show();
+
+         if (percentage >79){
+            $("#comments").text("Excellent!");
+        }else if(percentage <80 && percentage >39){
+            $("#comments").text("Fairly Done.");
+        }else{
+            $("#comments").text("You have failed redo the test");
+        };
      });
      $('#userprofile').submit(function(event){
          var name= $("#Name").val();
-         var class= $("#class").val();
+         var stream= $("#stream").val();
          var adm= $("#adm").val();
          var date= $("#date").val();
-     }
+     });
+
+     
   });
  
